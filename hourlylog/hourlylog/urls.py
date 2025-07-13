d566d251eac0e641b12d584bfc45f00ad75208fa
Arnav <arnavnpatil@gmail.com>
2025-07-13 20:54:47 +0530

    added login logout functionality

--- hourlylog/hourlylog/urls.py
+++ hourlylog/hourlylog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from tracker import views as tracker_views 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # for login/logout
+    path('signup/', tracker_views.signup, name='signup'),
     path('', include('tracker.urls')),
 ]
--- hourlylog/tracker/views.py
+++ hourlylog/tracker/views.py
@@ -1,12 +1,28 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import DayLog, HourLog
 from datetime import date as dt_date
 
+
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+@login_required
 def log_view(request):
     date_str = request.GET.get('date', dt_date.today().isoformat())
     date_obj = dt_date.fromisoformat(date_str)
 
-    day_log, created = DayLog.objects.get_or_create(date=date_obj)
+    day_log, created = DayLog.objects.get_or_create(user=request.user, date=date_obj)
 
     if request.method == 'POST':
         for hour in range(24):
