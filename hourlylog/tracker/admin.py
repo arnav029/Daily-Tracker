9b543f30c5b3f5a72ec2a33ed75fbfd78445a87c
Arnav <arnavnpatil@gmail.com>
2025-08-15 13:47:37 +0530

    fix(3): Deletd previous files

--- hourlylog/tracker/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import DayLog, HourLog
-
-admin.site.register(DayLog)
-admin.site.register(HourLog)
--- hourlylog/tracker/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import log_view
-
-urlpatterns = [
-    path('', log_view, name='log'),
-]
