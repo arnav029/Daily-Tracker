c3901c82c0bb7d2f991f8af468a60df4bd91d30c
Arnav <arnavnpatil@gmail.com>
2025-08-15 13:46:09 +0530

    Enhancement(2): Initial Deployement

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-l$806-3)%_!*h82w(-$6m=@(^--3_ac(3_y!^5*$lpus2wvw$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
